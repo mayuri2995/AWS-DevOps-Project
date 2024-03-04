f583f8585900543da170d69437e6647fba5135fd
mayuri2995 <113793056+mayuri2995@users.noreply.github.com>
2024-03-04 19:02:25 +0530

    Update DataAccess.java

--- src/main/java/com/microsoft/example/DataAccess.java
+++ src/main/java/com/microsoft/example/DataAccess.java
@@ -22,7 +22,7 @@ public class DataAccess
 {
 	// Some database-specific details we'll need
 	private static final String DB_DRIVER = "com.mysql.jdbc.Driver";
-	private static final String DB_URL = "jdbc:mysql://db:3306/MyShuttleDb";
+	private static final String DB_URL = "jdbc:mysql://alm.ctsiom60ws11.us-west-2.rds.amazonaws.com:3306/alm";
 	private static final String DB_USER = "user";
 	private static final String DB_PASS = "password";
 	
@@ -32,7 +32,7 @@ public class DataAccess
 			// Bootstrap driver into JVM
 			Class.forName(DB_DRIVER);
 
-			String conStr = System.getenv("MYSQLCONNSTR_MyShuttleDb");
+			String conStr = System.getenv("MYSQLCONNSTR_alm");
 			if (conStr == null || conStr.trim().length() == 0) {
 				theConnection = DriverManager.getConnection(DB_URL, DB_USER, DB_PASS);
 			} else {
